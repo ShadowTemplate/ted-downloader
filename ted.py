4e3a08701899808dfd563676bc348719ee0356b3
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2018-04-26 15:34:44 +0200

    Alpha version with JSON parsing

---
+++ ted.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+
+import json
+import traceback
+import urllib.request
+
+
+def parse_page(driver):
+    script_clue = "q(\"talkPage.init\","
+    try:
+        for script in driver.find_elements_by_tag_name("script"):
+            content = script.get_attribute("innerHTML")
+            if content.startswith(script_clue):
+                content = content.lstrip(script_clue).rstrip(")")
+                json_content = json.loads(content)
+                title = json_content["__INITIAL_DATA__"]["name"]
+                download_url = json_content["__INITIAL_DATA__"]["media"][
+                    "internal"]["podcast-high-en"]["uri"]
+                return title, download_url
+    except Exception:
+        print(traceback.format_exc())
+        print("Unable to parse page. Stopping.")
+        exit(-1)
+
+
+def main(url):
+    print("Processing URL %s..." % url)
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(url)
+    title, download_url = parse_page(driver)
+    print("TED talk: {}\nDownload URL: {}".format(title, download_url))
+    file_title = title + ".mp4"
+    print("Downloading file {}...".format(file_title))
+    try:
+        urllib.request.urlretrieve(download_url, file_title)
+        print("Download completed.")
+    except Exception:
+        print(traceback.format_exc())
+        print("Unable to download video. Stopping.")
+        exit(-1)
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
